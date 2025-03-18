ab217f0da0dfe5cb8ffc9b9f4c7b09a107eb9901
mingupup <2539823901@qq.com>
2025-03-18 11:11:07 +0800

    成功读取模型配置

---
+++ MCP-Studio/Models/ChatModelConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCP_Studio.Models
+{
+    public class ChatModelConfig
+    {
+        public string ModelID { get; set; }
+        public string ApiKey { get; set; }
+        public string BaseURL { get; set; }
+    }
+}
--- MCP-Studio/ViewModels/ChatViewModel.cs
+++ MCP-Studio/ViewModels/ChatViewModel.cs
@@ -1,4 +1,9 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MCP_Studio.Models;
+using System.Text.Json;
+using System;
+using System.IO;
 
 namespace MCP_Studio.ViewModels;
 
@@ -7,5 +12,20 @@ public partial class ChatViewModel : ViewModelBase
     public ChatViewModel()
     {
         
+    }
+
+    [RelayCommand]
+    private void Test()
+    {
+        try
+        {
+            string jsonString = File.ReadAllText("ChatModelSettings.json");
+            var chatModelConfig = JsonSerializer.Deserialize<ChatModelConfig>(jsonString);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error parsing settings: {ex.Message}");
+        }
+
     }
 } 
